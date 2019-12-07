7638540771ced62a7ccc5763e27d581dc88fb0ca
Amedeo Pro <amedeopro@me.com>
2019-12-07 17:40:37 +0100

    merge-conflict

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -4,8 +4,22 @@ namespace App\Http\Middleware;
 
 use Closure;
 
+<<<<<<< HEAD
 class CorsMiddleware {
     public function handle($request, Closure $next) {
+=======
+class CorsMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+>>>>>>> commit
         return $next($request)
             ->header('Access-Control-Allow-Origin', '*')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
