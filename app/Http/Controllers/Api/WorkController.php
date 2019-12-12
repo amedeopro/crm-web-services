26092578ff608dc707dee676a6492619bd757926
Amedeo Pro <amedeopro@me.com>
2019-12-12 16:47:34 +0100

    nuovo test per inserimento dati su tabella pivot

--- app/Http/Controllers/Api/WorkController.php
+++ app/Http/Controllers/Api/WorkController.php
@@ -62,7 +62,8 @@ class WorkController extends Controller
         $newWork->fill($validatedData);
         $newWork->save();
 	
-	CustomerWork::create(['customer_id' => '','work_id' => '','user_id' => '']);
+//	CustomerWork::create(['customer_id' => '','work_id' => '','user_id' => '']);
+$newWork->customers()->attach('customer_id',['user_id'=>'user_id_value']);
 	//$newWork->customers()->sync(['user_id','customer_id']);
 
        //  $newWork->users()->attach('user_id');
