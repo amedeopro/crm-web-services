7a83920eb0ed7ff02067b8de06f6bea8d7b1a7d6
Amedeo Pro <amedeopro@me.com>
2019-12-24 17:58:06 +0100

    preparati tutti i servizi di login e logout

--- app/User.php
+++ app/User.php
@@ -3,10 +3,11 @@
 namespace App;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
 
@@ -43,5 +44,24 @@ class User extends Authenticatable
 
     public function works(){
         return $this->belongsToMany(Work::class)->withTimestamps();
+    }
+
+    /**
+     * @return mixed
+     */
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -13,8 +13,16 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+Route::group(['prefix' => 'auth', 'namespace' => 'Auth'], function(){
+    Route::post('signin', 'SignInController');
+    Route::post('signout', 'SignOutController');
+
+    Route::get('me', 'MeController');
+
 });
 
 // visualizzazione
